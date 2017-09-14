9464f2023421bff31317836e18cf7119fabf48ea
DavidMwangi <david.mathenge98@gmail.com>
2017-09-14 14:18:14 +0300

    Password check

---
+++ password.py
@@ -0,0 +1,13 @@
+password = ''
+
+while password != 'python123':
+
+    password = input('> ')
+
+    if password == 'python123':
+
+        print('You are logged in')
+
+    else:
+
+        print('Sorry, try again!')
